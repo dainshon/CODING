3eb6240870403a9e5a9612c7da4943f0af7b6f03
dainshon <81453127+dainshon@users.noreply.github.com>
2022-08-13 13:21:30 +0900

    [level 1] Title: 음양 더하기, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,10 @@
+def solution(absolutes, signs):
+    answer=0
+    test=0
+    for i in range(len(absolutes)):
+        if(signs[i]==True):
+            answer+=absolutes[i]
+        else:
+            answer+=(-1)*absolutes[i]
+
+    return answer
